363eab48da019470eb0acf0df4d0db150ec76fd7
Ity_tell <nickkolomietsdooor@gmail.com>
2024-07-09 12:38:42 +0300

    main menu update

--- main.py
+++ main.py
@@ -16,21 +16,25 @@ class Game:
         self.fps = settings["fps"]
         self.clock = pygame.time.Clock()
         self.running = True
-        self.main_menu = MainMenu(self.screen)
+        self.main_menu = Main_menu(self.screen)
+        self.game_started = False
 
     def run(self):
         
 
         while self.running:
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for button in self.main_menu.buttons:
+                        if button.rect.collidepoint(event.pos):
+                            self.game_started, self.running = button.click()
 
-            self.screen.fill("blue")
-            self.main_menu.run(self.screen)
+            self.main_menu.run()
             pygame.display.flip()
-
-        self.clock.tick(self.fps)  
+            self.clock.tick(self.fps)  
 
         pygame.quit()
 Game().run()
--- main_menu.py
+++ main_menu.py
@@ -1,13 +1,18 @@
 
 import pygame
 pygame.font.init() 
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
+my_font = pygame.font.SysFont('Comic Sans MS', 100)
 
 class Button:
 
-    def __init__(self, text, function) -> None:
-        self.text = my_font.render(text, False, "black")
+    indantation = 0
+
+    def __init__(self, text, function, cords) -> None:
+        self.text = my_font.render(text, False, "white")
         self.function = function
+        rect = self.text.get_rect()
+        self.rect = pygame.Rect(cords[0] - 100 - rect.left, cords[1] + Button.indantation, rect.width, rect.height)
+        Button.indantation += rect.height
     
 
     def draw(self, screen, cords):
@@ -15,26 +20,32 @@ class Button:
 
 
     def click(self):
-        self.function()
+        return self.function()
 
 
 
-class MainMenu:
+class Main_menu:
     
         def __init__(self, screen) -> None:
             self.buttons = [
-                Button("Start", self.start_game),
-                Button("Exit", self.exit)
+                Button("Start", self.start_game, (screen.get_width() / 2, screen.get_height() / 4)),
+                Button("Exit", self.exit, (screen.get_width() / 2, screen.get_height() / 4))
             ]
             self.running = True
+            self.screen = screen
+            self.width = screen.get_width()
+            self.height = screen.get_height()
     
         def start_game(self):
-            pass
+            return True, True
 
         def exit(self):
             self.running = False
+            return False, False
     
-        def run(self, screen):
-            for i, button in enumerate(self.buttons):
-                button.draw(screen, (100, 100 + i * 50))
+        def run(self):
+            if self.running:
+                self.screen.fill("blue")
+                for button in self.buttons:
+                    button.draw(self.screen, (button.rect.x, button.rect.y))
 
