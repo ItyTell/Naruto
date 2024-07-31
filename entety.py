b1a59b6d424aa6106a1f49e946ca362866027149
Ity_tell <nickkolomietsdooor@gmail.com>
2024-07-31 21:31:01 +0300

    anim zagotovka

--- entety.py
+++ entety.py
@@ -4,6 +4,14 @@ class Entety:
     def __init__(self, name, health):
         self.name = name
         self.health = health
+        self.controller = Animation_controller()
 
     def take_damage(self, damage):
         self.health -= damage
+
+
+class Animation_controller():
+
+    def __init__(self):
+        pass
+        
